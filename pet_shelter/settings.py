c9fd4cb27e10f8e51f79c24ae9f6914da8adea4a
loadev2 <loadev@gmail.com>
2020-06-24 22:03:05 +0300

    add postresql (local)

--- pet_shelter/settings.py
+++ pet_shelter/settings.py
@@ -74,10 +74,22 @@ WSGI_APPLICATION = 'pet_shelter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
